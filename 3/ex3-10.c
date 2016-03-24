e0bd1937fdf19ffec3fa783db6a5b5abb3581634
tanakamariko <gshm10218@gmail.com>
2016-03-24 18:21:26 +0900

    use conditional operator instead of if statement

--- 3/ex3-10.c
+++ 3/ex3-10.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-  int a, b;
+  int a, b, diff;
 
   printf("二つの整数を入力してください。\n");
   printf("整数1:"); scanf("%d", &a);
   printf("整数2:"); scanf("%d", &b);
 
-  if(a < b) printf("それらの差は%dです。\n", b - a);
-  else printf("それらの差は%dです。\n", a - b);
+  diff = (a < b ) ?  b - a : a - b;
+
+  printf("それらの差は%dです。\n", diff);
 
   return 0;
 }
