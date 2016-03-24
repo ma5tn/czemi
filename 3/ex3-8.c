7d7d23f25dc5710d3da624173622f600629086c1
tanakamariko <gshm10218@gmail.com>
2016-03-24 17:42:08 +0900

    Replace A,B,C with 1,2,3

--- 3/ex3-8.c
+++ 3/ex3-8.c
@@ -4,9 +4,9 @@ int main(void){
   int a, b, c, min;
 
   printf("三つの整数を入力してください。\n");
-  printf("整数A:"); scanf("%d", &a);
-  printf("整数B:"); scanf("%d", &b);
-  printf("整数c:"); scanf("%d", &c);
+  printf("整数1:"); scanf("%d", &a);
+  printf("整数2:"); scanf("%d", &b);
+  printf("整数3:"); scanf("%d", &c);
 
   min = a;
   if(b < min) min = b;
