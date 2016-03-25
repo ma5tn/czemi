4006843c3d1773d846d8521fc639a52b033dc79e
tanakamariko <gshm10218@gmail.com>
2016-03-25 15:50:53 +0900

    add ex4-2.c

---
+++ 4/ex4-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+  int n;
+
+  do {
+    printf("非負の整数を入力してください:"); scanf("%d", &n);
+    if(n < 0) printf("負の数を入力しないでください。\n");
+  } while (n < 0);
+
+  printf("%d を逆から読むと ", n);
+
+  do {
+    printf("%d", n % 10);
+    n = n/ 10;
+  } while (0 < n);
+
+  printf(" です。\n");
+
+  return 0;
+}
