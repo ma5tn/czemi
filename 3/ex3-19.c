6b182f96ada63554aee2e6031b696323e27295d5
tanakamariko <gshm10218@gmail.com>
2016-03-25 15:01:23 +0900

    add ex3-19.c

---
+++ 3/ex3-19.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void){
+  int month;
+
+  printf("何月ですか:"); scanf("%d", &month);
+
+  switch (month) {
+    case 1:
+             printf("冬です。\n");
+               break;
+    case 2:
+             printf("冬です。\n");
+               break;
+    case 3:
+             printf("春です。\n");
+               break;
+    case 4:
+             printf("春です。\n");
+               break;
+    case 5:
+             printf("春です。\n");
+               break;
+    case 6:
+             printf("夏です。\n");
+               break;
+    case 7:
+             printf("夏です。\n");
+               break;
+    case 8:
+             printf("夏です。\n");
+               break;
+    case 9:
+             printf("秋です。\n");
+               break;
+    case 10:
+             printf("秋です。\n");
+               break;
+    case 11:
+             printf("秋です。\n");
+               break;
+    case 12:
+             printf("冬です。\n");
+               break;
+
+  }
+
+  return 0;
+}
