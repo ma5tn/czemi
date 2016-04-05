c5703348f62214273600fa0649b6e48530b0f740
tanakamariko <gshm10218@gmail.com>
2016-04-05 18:04:03 +0900

    fixed ex4-26.c

--- 4/ex4-26.c
+++ 4/ex4-26.c
@@ -4,7 +4,7 @@ int main(void){
   int n, i, j;
   printf("何段ですか:"); scanf("%d", &n);
 
-  for ( i = 0; i <= n; i++) {
+  for ( i = 0; i < n; i++) {
     for ( j = 0; j < i; j++) {
       printf(" ");
     }
