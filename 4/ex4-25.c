36d54eeba0b9792a35ac0b0ee307b9703af54cf4
tanakamariko <gshm10218@gmail.com>
2016-04-05 17:50:55 +0900

    add ex4-25.c

---
+++ 4/ex4-25.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+  int n, i, j;
+  printf("何段ですか:"); scanf("%d", &n);
+
+  for ( i = 0; i <= n; i++) {
+    for ( j = 1; j <= n - i; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
