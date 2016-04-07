429c34f012b0f3c57ef79d7261511bdf582ce907
tanakamariko <gshm10218@gmail.com>
2016-04-07 17:39:53 +0900

    replace N with 5

--- 5/ex5-11.c
+++ 5/ex5-11.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#define N 5
+#define 5 5
 
 int main(void){
-  int hight[N], i, sum;
+  int hight[5], i, sum;
 
   printf("5人の学生の身長を入力してください。\n");
-  for ( i = 0; i < N; i++) {
+  for ( i = 0; i < 5; i++) {
     printf("student %d：", i + 1); scanf("%d", &hight[i]);
   }
 
   sum = 0;
 
-  for ( i = 0; i < N; i++) {
+  for ( i = 0; i < 5; i++) {
     sum += hight[i];
   }
 
-  printf("5人の学生の平均身長は%.2fです。\n", (double)sum/N);
+  printf("5人の学生の平均身長は%.2fです。\n", (double)sum/5);
 
   return 0;
 }
